dcb117cc98e0293896d7a899fe1e10d24fb19eaf
Eze Onyebuchi <eze4acme@gmail.com>
2023-06-29 16:29:38 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hi');
+const months = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+const daysOfWeek = [
+  "Sunday", "Monday", "Tuesday", "Wednesday",
+  "Thursday", "Friday", "Saturday"
+];
