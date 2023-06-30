fe0e2f47b6104b1e92ffa090fc917c73d4ab2ac4
Eze Onyebuchi <eze4acme@gmail.com>
2023-06-30 10:07:02 +0100

    update index.js
    update index.js

--- index.js
+++ index.js
@@ -7,20 +7,40 @@ const daysOfWeek = [
   "Sunday", "Monday", "Tuesday", "Wednesday",
   "Thursday", "Friday", "Saturday"
 ];
+const subTitleEl = document.querySelector('.sub_title')
+const date = new Date()
+const month = date.getMonth() 
+const dayOfWeek = date.getDay()
 
-const dayOfWeek = new Date().getDay()
-const month = new Date().getMonth()
-console.log(daysOfWeek[dayOfWeek]);
-console.log(months[month]);
-console.log(new Date().getDate());
-console.log(new Date().getFullYear());
+subTitleEl.textContent = `giveaway ends on ${daysOfWeek[dayOfWeek]}, ${date.getDate()} ${months[month]} ${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}am`
+function countdown(targetDate) {
+    const daysEl = document.getElementById('days')
+    const hoursEl = document.getElementById('hours')
+    const minsEl = document.getElementById('mins')
+    const secsEl = document.getElementById('secs')
 
-console.log( Math.floor((new Date().getTime() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-console.log( Math.floor((new Date().getTime() % (1000 * 60 * 60)) / (1000 * 60 )));
+  let timer = setInterval(function() {
+    const now = new Date().getTime();
+    const timeRemaining = targetDate - now;
+
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    daysEl.textContent = `${days}`
+    hoursEl.textContent = `${hours}`
+    minsEl.textContent = `${minutes}`
+    secsEl.textContent = `${seconds}`
+
+    if (timeRemaining < 0) {
+      clearInterval(timer);
+      console.log("Countdown complete!");
+    }
+  }, 1000);
+}
+
+const targetDate = new Date("December 31, 2023 23:59:59").getTime();
+countdown(targetDate);
 
 
-console.log(new Date().getTime());
-const hours = new Date().getHours();
-const mins = new Date().getMinutes() 
-console.log(mins);
-console.log(hours);
